d3f3007e04eed2e8a15e6370031d4772c15b54d6
Valeria Garcia <valeriabeserragarcia@gmail.com>
2021-11-24 11:07:08 -0300

    primeiro commit

---
+++ README.txt
@@ -0,0 +1,22 @@
+Cadastro de Usuários
+ASP.NET CORE 5.0 C# Com Banco de dados MYSQL
+
+Criando cadastro simples usando usando ASP.Net Core 5.0 com C# e gerar um banco de dados MySQL
+
+Instalar as bibliotecas:
+Microsoft.EntityFrameworkCore
+Microsoft.EntityFrameworkCore.Tools
+5.00
+
+MySql.EntityFrameworkCore
+5.0
+
+
+-------------------------------------------
+iniciar o MySQL
+serviços -> mysql -> botão direito -> iniciar
+
+digitar o comando no MySQL 
+	show databases;
+	use webmysqlteste
+	select * from usuario
---
+++ WebMySQL/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMySQL.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Celular")]
+        [Column("Celular")]
+        public string Celular { get; set; }
+
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+    }
+}
